3568ea268cb8de894e95c831481d9e7e1830e124
Andy Toth <ondrej.toth@code.berlin>
2020-02-26 19:10:42 +0100

    list of restaurants in html

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'This is the homepage'
+@app.route("/")
+@app.route("/<restaurant>")
+def index(restaurant=None):
+    return render_template('restaurant.html', restaurant=restaurant)
+
+
+@app.route("/list")
+def list():
+    with open ('restaurants.txt', 'r') as f:
+        content = f.read()
+    return render_template("list.html", content=content)
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
 
-    
